d05c4e989ea9f0a0578cee14182a53b0f7a888fa
simar011 <93173580+simar011@users.noreply.github.com>
2021-12-22 17:33:32 +0530

    Add files via upload

--- Session 25.py
+++ Session 25.py
@@ -5,7 +5,7 @@ class Student:
         self.student_gender = input("Enter the student gender: ")
 
     def display(self):
-        print("Enter the student name: ",self.student_name)
+        print("\n Enter the student name: ",self.student_name)
         print("Enter the student age; ",self.student_age)
         print("Enter the student gender: ",self.student_gender)
 
@@ -13,11 +13,32 @@ class Marks:
     def __init__(self):
         self.grade = input("Enter the grade of the student: ")
         print("------- Evaluate Marks per subject --------")
-        self.english = input("Marks in English subject: ")
-        self.maths = input("Marks in Maths subject: ")
-        self.physics = input("Marks in Physics subject: ")
-        self.chemistry = input("Marks in Chemistry subject: ")
-        self.computer = input("Marks in Computer subject: ")
+        self.english = int(input("Marks in English subject: "))
+        self.maths = int(input("Marks in Maths subject: "))
+        self.physics = int(input("Marks in Physics subject: "))
+        self.chemistry = int(input("Marks in Chemistry subject: "))
+        self.computer = int(input("Marks in Computer subject: "))
+
+    def display(self):
+        self.total = self.english + self.maths + self.physics + self.chemistry + self.computer
+        print("Total marks: ",self.total)
+
+class Data(Student,Marks):
+    def __init__(self):
+        Student.__init__(self)
+        Marks.__init__(self)
 
     def result(self):
-        print("Total Evaluated Marks: ",self.english + self.maths + self.physics + self.chemistry + self.computer)
+        Marks.display(self)
+
+S1 = Data()
+S1.result()
+S2 = Data()
+S2.result()
+S3 = Data()
+S3.result()
+S4 = Data()
+S4.result()
+
+print("You can create more objects to add objects to add more student data")
+    
